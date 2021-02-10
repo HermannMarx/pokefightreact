cdd22a85a17a0a7906658b21c56ea27230c84d84
Stefan Klarmann <klarmann1@googlemail.com>
2021-02-10 12:17:49 +0100

    added three mandatory routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import { Button } from "@material-ui/core";
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/pokemon/:id/:info">
+          <ViewPokemonInfo />
+        </Route>
+        <Route path="/pokemon/:id">
+          <ViewPokemon />
+        </Route>
+        <Route path="/pokemon">
+          <ViewPokemonAll />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
