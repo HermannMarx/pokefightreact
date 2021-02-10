925f7180077d89fa34b6c000f428924b9fe5f532
Stefan Klarmann <klarmann1@googlemail.com>
2021-02-10 23:13:03 +0100

    added srcImageUrl to get matching images by id

--- src/components/Pokemon/index.js
+++ src/components/Pokemon/index.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React from 'react';
 
-import "./index.css";
+import './index.css';
 
 export default function Pokemon({ pokemon }) {
+  const srcImageUrl =
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' +
+    pokemon.id +
+    '.png';
   return (
     <div className="pokemon-card">
       {/*<img src={pokemon.image} alt="" />*/}
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/34.png"
-        alt=""
-      />
-      <p>{pokemon.name.english}</p>
+      <img src={srcImageUrl} alt="" />
+      <p>{pokemon.id}</p>
     </div>
   );
 }
