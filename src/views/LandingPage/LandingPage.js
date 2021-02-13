5912c6e509b3242706531e345fbdda531c2ea20e
Stefan Klarmann <klarmann1@googlemail.com>
2021-02-13 13:58:15 +0100

    landing page styling

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -4,12 +4,21 @@ import './styles.css';
 
 export default function LandingPage() {
   return (
-    <div className="landingPage">
-      <Link to="/pokemon">
-        <img src="https://fontmeme.com/permalink/210213/cf386625e046741661c108ec110e8dd3.png" />
-        <img src="https://fontmeme.com/permalink/210213/4e78adebf701eeac4f27736ba96b3d3d.png" />
-        <img src="https://fontmeme.com/permalink/210213/dabd971ed138cccc7c995dab17ea2a08.png" />
-      </Link>
-    </div>
+    <Link to="/pokemon">
+      <div className="landingPage">
+        <img
+          className="img-welcome"
+          src="https://fontmeme.com/permalink/210213/cf386625e046741661c108ec110e8dd3.png"
+        />
+        <img
+          className="img-title"
+          src="https://fontmeme.com/permalink/210213/4e78adebf701eeac4f27736ba96b3d3d.png"
+        />
+        <img
+          className="img-press"
+          src="https://fontmeme.com/permalink/210213/dabd971ed138cccc7c995dab17ea2a08.png"
+        />
+      </div>
+    </Link>
   );
 }
