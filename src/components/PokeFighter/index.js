05d4449dbd7e9d47abe50d3b6b69c7db07912655
Hermann Marx <marxhermann@web.de>
2021-02-18 16:24:04 +0100

    test

--- src/components/PokeFighter/index.js
+++ src/components/PokeFighter/index.js
@@ -1,24 +1,26 @@
 import './styles.css';
-const PokeFighter = ({ name, fighter, id, url, HP, Attack, Defense }) => {
-  console.log(fighter);
-  console.log('This is Id' + id);
-  console.log('This is URL ' + url);
-
+const PokeFighter = ({
+  name,
+  fighter,
+  id,
+  url,
+  HP,
+  fightHP,
+  Attack,
+  Defense
+}) => {
   return (
     <div className="pokeFighter">
       {name}
       <div className="pokeCenter">
         <img src={url} alt="pokefighter" />
         <div className="pokeInfo">
-          <p>HP: {HP}</p>
+          <p>
+            HP: {fightHP}/{HP}
+          </p>
           <p>Attack: {Attack}</p>
           <p>Defense: {Defense}</p>
         </div>
-      </div>
-      <div className="fightButtons">
-        <button>Attack</button>
-        <button>Defend</button>
-        <button>Run</button>
       </div>
     </div>
   );
