aaddfd59a342a9fcf25a70c3064cc5a6f099b924
Stefan Klarmann <klarmann1@googlemail.com>
2021-02-18 21:39:49 +0100

    buttons, button alignment

--- src/components/Pokemon/index.js
+++ src/components/Pokemon/index.js
@@ -13,22 +13,24 @@ export default function Pokemon({ index, pokemon, choose1, choose2 }) {
         <br></br>
         <span>{pokemon.name.english}</span>
       </Link>
-      <button
-        className="btn btn-choose1"
-        onClick={() => {
-          choose1(index);
-        }}
-      >
-        1
-      </button>
-      <button
-        className="btn btn-choose2"
-        onClick={() => {
-          choose2(index);
-        }}
-      >
-        2
-      </button>
+      <div className="btn-choose-container">
+        <button
+          className="btn btn-choose1"
+          onClick={() => {
+            choose1(index);
+          }}
+        >
+          Player 1
+        </button>
+        <button
+          className="btn btn-choose2"
+          onClick={() => {
+            choose2(index);
+          }}
+        >
+          Player 2
+        </button>
+      </div>
     </div>
   );
 }
